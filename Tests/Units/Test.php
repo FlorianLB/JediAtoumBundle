ecfb86e24010f3415d16dffd059ed61140cfc40e
Stéphane PY <py.stephane1@gmail.com>
2012-08-09 09:58:49 +0200

    Add WebTestCase, do not set the require of phar on the abstract class

--- Tests/Units/Test.php
+++ Tests/Units/Test.php
@@ -4,16 +4,11 @@ namespace Jedi\AtoumBundle\Tests\Units;
 
 use mageekguy\atoum;
 
-require_once __DIR__ . '/../../../../../vendor/mageekguy.atoum.phar';
-
-/**
- * Test
- *
- * @uses atoum
- * @author Stephane PY <py.stephane1@gmail.com>
- */
 abstract class Test extends atoum\test
 {
+    /**
+     * @param atoum\factory $factory factory
+     */
     public function __construct(atoum\factory $factory = null)
     {
        $this->setTestNamespace('Tests\Units');
