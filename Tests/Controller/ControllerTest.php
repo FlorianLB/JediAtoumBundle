2cd534b61ac9c61ca58d4b535871b04c8b6ec32a
FlorianLB <flonqueubrochard@florianlb-mbp.dev.clever-age.net>
2012-10-16 16:11:39 +0200

    new class for test a controller

---
+++ Tests/Controller/ControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Jedi\AtoumBundle\Tests\Controller;
+
+use Jedi\AtoumBundle\Tests\Units\WebTestCase;
+use mageekguy\atoum;
+
+abstract class ControllerTest extends WebTestCase
+{
+    /**
+     * @param atoum\factory $factory factory
+     */
+    public function __construct(atoum\factory $factory = null)
+    {
+        parent::__construct($factory);
+        $this->setTestNamespace('Tests');
+    }
+}
+?>
