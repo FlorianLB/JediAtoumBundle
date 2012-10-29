086eec7d4f3b7b849a75238fd39b5c3e855c5be4
marmotz <rlittolff@gmail.com>
2012-10-29 18:08:20 +0100

    export getRandomString to its own class in Tests\Generator\String

--- Tests/Units/Test.php
+++ Tests/Units/Test.php
@@ -2,6 +2,7 @@
 
 namespace Jedi\AtoumBundle\Tests\Units;
 
+use Jedi\AtoumBundle\Tests\Generator;
 use mageekguy\atoum;
 
 abstract class Test extends atoum\test
@@ -15,13 +16,6 @@ abstract class Test extends atoum\test
         parent::__construct($factory);
     }
 
-
-    const CHARACTERS_ALPHA_LOWER  = 1;
-    const CHARACTERS_ALPHA_UPPER  = 2;
-    const CHARACTERS_ALPHA        = 3;
-    const CHARACTERS_NUMERIC      = 4;
-    const CHARACTERS_ALPHANUMERIC = 7;
-
     /**
      * Return a random string
      *
@@ -31,50 +25,22 @@ abstract class Test extends atoum\test
      * $this->getRandomString(32)
      *     => random alphanumeric string of length 32
      *
-     * $this->getRandomString(10, self::CHARACTERS_NUMERIC)
+     * $this->getRandomString(10, Generator\String::CHARACTERS_NUMERIC)
      *     => random numeric string of length 10
      *
-     * $this->getRandomString(10, self::CHARACTERS_NUMERIC + self::CHARACTERS_ALPHA_LOWER)
+     * $this->getRandomString(10, Generator\String::CHARACTERS_NUMERIC + Generator\String::CHARACTERS_ALPHA_LOWER)
      *     => random lower case alpha numeric string of length 10
      *
      * $this->getRandomString(32, '0123456789ABCDEF')
      *     => random hexadecimal string of length 32
      * 
      * @param  integer          $length
-     * @param  integer|string   $characters [description]
+     * @param  integer|string   $characters
      * 
      * @return string
      */
-    protected function getRandomString($length = null, $characters = self::CHARACTERS_ALPHANUMERIC)
+    protected function getRandomString($length = null, $characters = Generator\String::CHARACTERS_ALPHANUMERIC)
     {
-        if($length === null || $length < 0) {
-            $length = rand(8, 16);
-        }
-
-        if(is_int($characters)) {
-            $bits = $characters;
-            $characters = '';
-
-            if($bits & self::CHARACTERS_ALPHA_LOWER) {
-                $characters .= 'abcdefghijklmnopqrstuvwxyz';
-            }
-
-            if($bits & self::CHARACTERS_ALPHA_UPPER) {
-                $characters .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-            }
-
-            if($bits & self::CHARACTERS_NUMERIC) {
-                $characters .= '0123456789';
-            }
-        }
-
-        $string = '';
-        $charactersLength = strlen($characters);
-
-        for($i = 0; $i < $length; $i++) {
-            $string .= $characters[rand(0, $charactersLength - 1)];
-        }
-
-        return $string;
+        return Generator\String::generate($length, $characters);
     }
 }
